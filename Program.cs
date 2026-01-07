25d002e75dea2a2270c8397720be751886ce52c2
Vlad D. <vladdidyk1985@gmail.com>
2026-01-07 13:06:49 +0200

    mirror messaging logic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,50 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Bot;
-using System;
 
-var me = await BotConfig.GetMe();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+using var cts = new CancellationTokenSource();
+
+var bot = new TelegramBotClient(BotConfig.Token);
+
+var me = await bot.GetMe(); 
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<UpdateType>() 
+};
+
+bot.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cts.Token
+);
+
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    Console.WriteLine($"Received '{messageText}' in chat {message.Chat.Id}");
+
+    await botClient.SendMessage(
+        chatId: message.Chat.Id,
+        text: messageText,
+        cancellationToken: cancellationToken
+    );
+}
+
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    Console.WriteLine($"Error: {exception.Message}");
+    return Task.CompletedTask;
+}
--- obj/Debug/net10.0/bot_test.AssemblyInfo.cs
+++ obj/Debug/net10.0/bot_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bot_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8c9e38c5ff3c43ac65642ff0d637b74fef531d")]
 [assembly: System.Reflection.AssemblyProductAttribute("bot_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bot_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
